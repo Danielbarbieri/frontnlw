3cbd068868102d4f205cdb6490cdfd2bef03f44d
Daniel Barbieri <danielbarbierianalista@gmail.com>
2021-10-20 20:40:05 -0300

    42 do segundo

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,7 +1,50 @@
+import styles from './styles.module.scss';
+
+import logoImg from '../../assets/Logo DoWhile - 2021.png'
+
 export function MessageList(){
-    return(
-        <h1>
-            MessageList
-        </h1>
+    return (
+        <div className={styles.messageListWrapper}>
+            <img src={logoImg} alt="Dowhile2021" />
+
+            <ul className={styles.messageList}>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>
+                    Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
+                    </p>
+                    <div className={styles.messageUser}>
+                        <div className={ styles.userImage}>
+                            <img src="https://github.com/Danielbarbieri.png" alt="Daniel Barbieri" />
+                        </div>
+
+                    </div>
+                </li>
+
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>
+                    Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
+                    </p>
+                    <div className={styles.messageUser}>
+                        <div className={ styles.userImage}>
+                            <img src="https://github.com/Danielbarbieri.png" alt="Daniel Barbieri" />
+                        </div>
+
+                    </div>
+                </li>
+
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>
+                    Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
+                    </p>
+                    <div className={styles.messageUser}>
+                        <div className={ styles.userImage}>
+                            <img src="https://github.com/Danielbarbieri.png" alt="Daniel Barbieri" />
+                        </div>
+
+                    </div>
+                </li>
+
+            </ul>
+        </div>
     )
 }

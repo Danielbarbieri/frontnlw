98a8305db64a3b326af4fbab7d365702eeeb2184
Daniel Barbieri <danielbarbierianalista@gmail.com>
2021-10-21 18:21:34 -0300

    quinta..quase lá

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -4,13 +4,27 @@ import { api } from '../../services/api'
 import styles from './styles.module.scss';
 
 import logoImg from '../../assets/Logo DoWhile - 2021.png'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+
+type Message = {
+
+    id: string;
+    text: string;
+    user:{
+        name:string;
+        avatar_url:string;
+    }
+
+}
 
 export function MessageList(){
 
+    const [messages, setMessages] = useState<Message[]>([])
+
     useEffect(()=> {
-        api.get('messages/last3').then ( response => {
-            console.log(response.data);
+        api.get<Message[]>('messages/last3').then ( response => {
+            setMessages(response.data);
         })
     }, [])  
 
@@ -19,44 +33,27 @@ export function MessageList(){
             <img src={logoImg} alt="Dowhile2021" />
 
             <ul className={styles.messageList}>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>
-                    Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
-                    </p>
-                    <div className={styles.messageUser}>
-                        <div className={ styles.userImage}>
-                            <img src="https://github.com/Danielbarbieri.png" alt="Daniel Barbieri" />
-                        </div>
-                        <span>Daniel Barbieri</span>
+                { messages.map(message => {
 
-                    </div>
-                </li>
+                    return (
+                        <li key={message.id} className={styles.message}>
+                            <p className={styles.messageContent}>
+                            {message.text}
+                            </p>
+                            <div className={styles.messageUser}>
+                                <div className={ styles.userImage}>
+                                    <img src={message.user.avatar_url} alt={message.user.name} />
+                                </div>
+                                <span>{message.user.name}</span>
 
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>
-                    Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
-                    </p>
-                    <div className={styles.messageUser}>
-                        <div className={ styles.userImage}>
-                            <img src="https://github.com/Danielbarbieri.png" alt="Daniel Barbieri" />
-                        </div>
-                        <span>Daniel Faria</span>
+                            </div>
+                        </li>
+                    );
+                })}
 
-                    </div>
-                </li>
+                
 
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>
-                    Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
-                    </p>
-                    <div className={styles.messageUser}>
-                        <div className={ styles.userImage}>
-                            <img src="https://github.com/Danielbarbieri.png" alt="Daniel Barbieri" />
-                        </div>
-                        <span>Joaquim Barbieri</span>
 
-                    </div>
-                </li>
 
             </ul>
         </div>
